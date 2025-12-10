638d4bc55838934747c7707c8215d8de1bec9ccb
Marc Terradas <terradas.marc@gmail.com>
2025-12-10 11:42:15 +0100

    Update styles

--- app/src/Footer.vue
+++ app/src/Footer.vue
@@ -3,23 +3,33 @@ import { RouterLink } from 'vue-router'
 </script>
 
 <template>
-  <footer>
-    <nav>
-      <ul>
-        <li>
-          <RouterLink to="/">Home</RouterLink>
+  <footer class="footer">
+    <nav class="footer__nav">
+      <ul class="footer__list">
+        <li class="footer__item">
+          <RouterLink to="/" class="footer__link">
+            <img src="/house.svg" alt="Home" width="20" height="20" />
+          </RouterLink>
         </li>
-        <li>
-          <RouterLink to="/journal">Journal</RouterLink>
+        <li class="footer__item">
+          <RouterLink to="/journal" class="footer__link">
+            <img src="/book.svg" alt="Journal" width="20" height="20" />
+          </RouterLink>
         </li>
-        <li>
-          <RouterLink to="/stats">Stats</RouterLink>
+        <li class="footer__item">
+          <RouterLink to="/stats" class="footer__link">
+            <img src="/chart.svg" alt="Stats" width="20" height="20" />
+          </RouterLink>
         </li>
-        <li>
-          <RouterLink to="/social">Social</RouterLink>
+        <li class="footer__item">
+          <RouterLink to="/social" class="footer__link">
+            <img src="/users.svg" alt="Social" width="20" height="20" />
+          </RouterLink>
         </li>
-        <li>
-          <RouterLink to="/profile">Profile</RouterLink>
+        <li class="footer__item">
+          <RouterLink to="/profile" class="footer__link">
+            <img src="/user.svg" alt="Profile" width="20" height="20" />
+          </RouterLink>
         </li>
       </ul>
     </nav>
@@ -27,7 +37,7 @@ import { RouterLink } from 'vue-router'
 </template>
 
 <style scoped>
-footer {
+.footer {
   position: fixed;
   bottom: 0;
   left: 0;
@@ -35,19 +45,22 @@ footer {
   background-color: var(--pico-background-color);
   border-top: 1px solid var(--pico-muted-border-color);
   padding: 0.5rem 1rem;
-  z-index: 99;
 }
 
-nav ul {
+.footer__list {
   display: flex;
   justify-content: space-around;
-  width: 100%;
   margin: 0;
   padding: 0;
+  width: 100%;
 }
 
-nav li {
+.footer__item {
   list-style: none;
-  padding: 0;
+}
+
+.footer__link {
+  text-decoration: none;
+  padding: 0.5rem;
 }
 </style>
--- app/src/NavBar.vue
+++ app/src/NavBar.vue
@@ -1,65 +1,35 @@
-<script setup>
-import { RouterLink } from 'vue-router'
-</script>
-
-<template>
-  <header class="nav">
-    <div class="nav__left">
-      <RouterLink to="/" class="brand">Fitness Journal</RouterLink>
-    </div>
-
-    <div class="nav__right">
-      <button class="icon" aria-label="Messages" title="Messages" type="button">
-        <img src="/message.svg" alt="Messages" width="20" height="20" />
+f<template>
+  <header class="navbar">
+      <button class="navbar__icon-button" aria-label="Messages" title="Messages" type="button">
+        <img class="navbar__icon" src="/message.svg" alt="Messages" width="20" height="20" />
       </button>
 
-      <button class="icon" aria-label="Notifications" title="Notifications" type="button">
-        <img src="/bell.svg" alt="Notifications" width="20" height="20" />
+      <button class="navbar__icon-button" aria-label="Notifications" title="Notifications" type="button">
+        <img class="navbar__icon" src="/bell.svg" alt="Notifications" width="20" height="20" />
       </button>
-    </div>
   </header>
 </template>
 
 <style scoped>
-.nav {
+.navbar {
   display: flex;
   align-items: center;
-  justify-content: space-between;
+  justify-content: flex-end;
   border-bottom: 1px solid var(--pico-muted-border-color);
-  background: var(--pico-background-color);
-  position: sticky;
-  top: 0;
 }
 
-.brand {
-  font-weight: 600;
-  color: inherit;
-  text-decoration: none;
+.navbar__brand {
+   text-decoration: none;
 }
 
-.nav__right {
-  display: flex;
-  gap: 0.5rem;
-  align-items: center;
-}
-
-.icon {
-  background: transparent;
+.navbar__icon-button {
+  background: none;
   border: none;
-  padding: 0.35rem;
-  display: inline-flex;
-  align-items: center;
-  justify-content: center;
+  padding: 0.5rem;
   cursor: pointer;
-  color: inherit;
 }
 
-.icon img {
+.navbar__icon {
   display: block;
-}
-
-.icon:focus {
-  outline: 2px solid Highlight;
-  border-radius: 4px;
 }
 </style>
